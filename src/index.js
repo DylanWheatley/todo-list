0e07ee448c9d326d77fe8b30e30641dac8249a4c
dylanwheatley <dylan.r.wheatley@gmail.com>
2023-06-17 15:51:16 -0400

    Psuedo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("tur");
+//to do list project
+
+//ability to make a project and display them
+//simple header, sidebar, and big open space
+
+//module for making a new project
+//title, description, due date, priority (color coordinated?) bonus: notes or checklist
+
+//module for switching between viewing each project
+//resturaunt page but without making a new module for each
+//module that takes in whichever project is clicked? maybe a factory inside this?
+
+//delete projects
+
+//local storage
